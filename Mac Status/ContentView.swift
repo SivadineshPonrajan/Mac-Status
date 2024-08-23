3d4be261a69226c1e80650af92937fb866135593
Sivadinesh Ponrajan <sivadinesh107@gmail.com>
2024-08-23 23:55:03 +0200

    Initial Commit

---
+++ Mac Status/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mac Status
+//
+//  Created by Sivadinesh Ponrajan on 23/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mac Status/Mac_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_StatusApp.swift
+//  Mac Status
+//
+//  Created by Sivadinesh Ponrajan on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Mac_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
